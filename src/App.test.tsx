b432a90847158503a3da7c715a6f674924e7e5c8
Joseph Greenfield <joseph.greenfield@parsons.com>
2021-12-22 14:02:24 -0500

    initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('data standby', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const div = screen.getByLabelText('data');
+  await waitFor(() => expect(div?.getAttribute('class')).toEqual('App standby'));
+});
+
+test('data sucess', async () => {
+  render(<App />);
+  const div = screen.getByLabelText('data');
+  await waitFor(() => expect(div?.getAttribute('class')).toEqual('App sucess'));
+});
+
+test('data fail', async () => {
+  render(<App />);
+  const div = screen.getByLabelText('data');
+  await waitFor(() => expect(div?.getAttribute('class')).toEqual('App fail'));
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
+  const [data, setData] = useState<string>('standby');
+  const [otherData, setOtherData] = useState<string>('standby');
+  const [changingValue, setChangingValue] = useState<boolean>(false);
+
+  const updateStatus = (): void => {
+    setTimeout(() => {
+        if (Math.random() > .5) {
+          setData('sucess');
+        } else {
+          setData('fail');
+        }
+    }, 0);
+  };
+
+  // Empty second argument means this will only be executed twice. (on mount and on unmount)
+  useEffect(() => {
+    console.log('executed');
+    updateStatus();
+    const updateInterval = setInterval(updateStatus, 5000);
+    return function cleanup() {
+      clearInterval(updateInterval);
+    };
+  }, []);
+
+  // Because we specify otherData as a dependency, useEffect will continue to execute every rerender.
+  // We want to do this alongside a guard statement in order to ensure execution of our code 
+  // until a certain value is met.
+  useEffect(() => {
+    if (otherData !== 'sucess') {
+      setTimeout(() => {
+        if (Math.random() > .5) {
+          setOtherData('sucess');
+        } else {
+          setOtherData('fail');
+        }
+      }, 5000);
+    }
+  }, [otherData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={`App ${data}`} aria-label='data'>{data} {otherData}</div>
+      <button onClick={() => setChangingValue(!changingValue)}>Click me to cause a rerender: {`${changingValue}`}</button>
+    </>
   );
 }
 
