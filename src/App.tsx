77f384ef85ee5545ebc320f66703bbefc630f650
Joseph Greenfield <joseph.greenfield@parsons.com>
2021-12-24 11:14:12 -0500

    prevent memory leaks

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,27 @@ function App(): JSX.Element {
   const [otherData, setOtherData] = useState<string>('standby');
   const [changingValue, setChangingValue] = useState<boolean>(false);
 
-  const updateStatus = (): void => {
+  const updateStatus = (isComponentMounted: any): void => {
     setTimeout(() => {
+      if (isComponentMounted.flag) {
         if (Math.random() > .5) {
           setData('sucess');
         } else {
           setData('fail');
         }
+      }
     }, 0);
   };
 
   // Empty second argument means this will only be executed twice. (on mount and on unmount)
+  // Can prevent memory leaks by useing a boolean flag. (If using a function, a normal boolean won't work)
   useEffect(() => {
+    let isComponentMounted = {flag: true};
     console.log('executed');
-    updateStatus();
+    updateStatus(isComponentMounted);
     const updateInterval = setInterval(updateStatus, 5000);
     return function cleanup() {
+      isComponentMounted.flag = false;
       clearInterval(updateInterval);
     };
   }, []);
@@ -30,15 +35,22 @@ function App(): JSX.Element {
   // We want to do this alongside a guard statement in order to ensure execution of our code 
   // until a certain value is met.
   useEffect(() => {
+    let isComponentMounted = true;
+  
     if (otherData !== 'sucess') {
       setTimeout(() => {
-        if (Math.random() > .5) {
-          setOtherData('sucess');
-        } else {
-          setOtherData('fail');
+        if (isComponentMounted) {
+          if (Math.random() > .5) {
+            setOtherData('sucess');
+          } else {
+            setOtherData('fail');
+          }
         }
       }, 5000);
     }
+    return () => {
+      isComponentMounted = false;
+    }; 
   }, [otherData]);
 
   return (
